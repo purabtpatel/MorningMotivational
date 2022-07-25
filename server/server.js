41132e9731b69d1179f934c00492a00840e1d6e0
Purab Patel <purabtpatel24@gmail.com>
2022-07-25 15:26:31 +0530

    Added basic express server
    backend will handle login/signup and fetch quotes from database

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get("/api", (req,res) => {
+    res.json({"users": ["user1","user2","user3"]})
+})
+app.listen(5000, ()=> {console.log("Server started on ")})
